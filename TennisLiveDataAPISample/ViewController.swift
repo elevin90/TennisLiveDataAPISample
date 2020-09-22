140e6895e6e9ee6d6995f543786d7b7ad615d411
Evgeniy Levin <yauheni.levin@apalon.com>
2020-09-22 12:40:05 +0300

    Initial Commit

---
+++ TennisLiveDataAPISample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TennisLiveDataAPISample
+//
+//  Created by AP Yauheni Levin on 22.09.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
